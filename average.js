5cbd68d6013a8a6abbe7d5a3ca59316615b0761a
ghigbie <gehigbie@gmail.com>
2017-02-10 00:23:09 +0000

    added meat of function

--- average.js
+++ average.js
@@ -2,5 +2,9 @@ var scores = [90, 98, 89, 100, 100, 86, 94];
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
 
 function averageScores(scoresArray){
-    for(i <)
+    var scores = 0;
+    for(var i = 0; i < scoresArray.length; i++){
+        scores+= scoresArray[i];
+        return scores/scores.length;
+    }
 }
